785fea6e4bfdaa5d40dd404373d6bb996900b5f0
Radzhab2024 <shiraev2002@gmail.com>
2024-08-21 15:24:03 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,16 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        length_of_matrix = []
+        matrix.append(length_of_matrix)
+        for j in range(m):
+            length_of_matrix.append(value)
+            #print(matrix[i])
+    return matrix
+result1 = get_matrix(2,2,10)
+#print(result1)
+result2 = get_matrix(3, 5,42)
+#print(result2)
+result3 = get_matrix(4, 2, 13)
+print(result1,'\n',result2,'\n',result3)
+
